99396ac8084eef8bfd83ab4821f3e0d064507720
Thesilentwindow <thesilentwindow@live.dk>
2014-10-17 08:59:30 +0200

    Update

--- mainMenu.cs
+++ mainMenu.cs
@@ -19,16 +19,7 @@ namespace Lager_System
             InitializeComponent();
         }
         
-        private const int CP_NOCLOSE_BUTTON = 0x200;
-        protected override CreateParams CreateParams
-        {
-            get
-            {
-                CreateParams myCp = base.CreateParams;
-                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
-                return myCp;
-            }
-        }
+
 
         private void Index_Load(object sender, EventArgs e)
         {
